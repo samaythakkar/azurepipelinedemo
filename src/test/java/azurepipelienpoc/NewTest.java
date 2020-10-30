b114d54a61d6e71fae8a31433e307e9d6e471d9d
MacBook Air <macbookair@MacBook-Air.local>
2020-10-31 02:34:44 +0530

    Added title tes

--- src/test/java/azurepipelienpoc/NewTest.java
+++ src/test/java/azurepipelienpoc/NewTest.java
@@ -7,7 +7,14 @@ public class NewTest {
   @Test
   public void urlTest() {
 	  String currentUrl =  HooksClass.getDriver().getCurrentUrl();
-	  System.out.println("CURRENT IS " +currentUrl);
-	  Assert.assertEquals(currentUrl, "https://www.google.com/");
+	  System.out.println("CURRENT URL IS " +currentUrl);
+	  Assert.assertEquals(currentUrl, "https://www.abc.com/");
+  }
+  
+  @Test
+  public void titleTest() {
+	  String currenTitle =  HooksClass.getDriver().getTitle();
+	  System.out.println("CURRENT TITLE IS " +currenTitle);
+	  Assert.assertEquals(currenTitle, "Google");
   }
 }
