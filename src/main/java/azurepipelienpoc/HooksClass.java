0ec16eaf6b180e4d8651c588340c9059ee5b50d8
MacBook Air <macbookair@MacBook-Air.local>
2020-10-31 22:31:04 +0530

    added testlistner and screenshots

--- src/main/java/azurepipelienpoc/HooksClass.java
+++ src/main/java/azurepipelienpoc/HooksClass.java
@@ -1,5 +1,11 @@
 package azurepipelienpoc;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,20 +17,49 @@ public class HooksClass {
 	public static WebDriver driver=null;
 
 
-	public static WebDriver getDriver() {
+	public static void initlializeDriver() {
 		WebDriver driver = null;
 		WebDriverManager.chromedriver().version("86.0.4240.22").setup();
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("start-maximized"); 
-//		options.addArguments("enable-automation"); 
-//		options.addArguments("--no-sandbox"); 
-//		options.addArguments("--disable-infobars");
-//		options.addArguments("--disable-dev-shm-usage");
-//		options.addArguments("--disable-browser-side-navigation"); 
-//		options.addArguments("--disable-gpu"); 
+		options.addArguments("start-maximized"); 
+		options.addArguments("enable-automation"); 
+		options.addArguments("--no-sandbox"); 
+		options.addArguments("--disable-infobars");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-browser-side-navigation"); 
+		options.addArguments("--disable-gpu"); 
 		driver = new ChromeDriver(options); 
 		driver.get("https://www.google.com/");
-		return driver;
+		HooksClass.driver = driver;
 
 	}
+	
+	public static WebDriver getDriver() {
+		WebDriver driver = null;
+		if (HooksClass.driver == null) {
+			HooksClass.initlializeDriver();
+			driver = HooksClass.driver;
+			
+		}else {
+			driver =  HooksClass.driver;
+		}
+		return driver;
+	}
+//	
+//	
+//	
+//	public static void takeScreenshot(WebDriver driver, String filePath) throws IOException {
+//		
+//		TakesScreenshot scrshot = ((TakesScreenshot)driver);
+//		
+//        File SrcFile=scrshot.getScreenshotAs(OutputType.FILE);
+//		
+//        File DestFile=new File(filePath);
+//
+//        FileUtils.copyFile(SrcFile, DestFile);
+//
+//	}
+	
+	
+	
 }
